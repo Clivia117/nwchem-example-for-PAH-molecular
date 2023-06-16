272438a5c96dd0c4b107f9f882f03dd668240b22
Clivia117 <80371902+Clivia117@users.noreply.github.com>
2023-06-16 10:58:31 +0200

    Update nwchem.sh

--- nwchem.sh
+++ nwchem.sh
@@ -2,15 +2,12 @@
 
 #SBATCH -J nwchem
 #SBATCH -n 20
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=lijun.pan@tuwien.ac.at
 
-module load nwchem openmpi3
+#you can change up script base on your calculator
 
-#uids=`awk 'NR<3000  {print $2}' ../database/uid4charge0.dat`
-uids=`awk '{print $1}' calc_dat/ef1.dat`
-#for uid in 309 346 631 2602 2844 2946 2969 3055 3096 3145 4003 ;do
+uids=`awk 'NR<3000  {print $2}' uid4charge0.dat`
+
 for uid in $uids ;do
   echo $uid
-  python asenwchem_ef.py  $uid 
+  python asenwchem_ef_opt.py  $uid 
 done

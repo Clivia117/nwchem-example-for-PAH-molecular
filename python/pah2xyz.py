b1a6fd5c058ae9f5301b95a972e7eb71c4dee796
Clivia117 <80371902+Clivia117@users.noreply.github.com>
2023-06-16 11:12:36 +0200

    Update pah2xyz.py

--- python/pah2xyz.py
+++ python/pah2xyz.py
@@ -5,12 +5,9 @@ from ase.io import read, write
 from pkg_resources import resource_filename
 from amespahdbpythonsuite.amespahdb import AmesPAHdb
 
-
 charge= 0
-mag=2
-q=charge
 js=f'pahdball_charge{charge}.json'
-"""
+
 # Read the database.
 xml = 'nasapahdb_all.xml'
 pahdb = AmesPAHdb(filename=xml,
@@ -19,7 +16,6 @@ pahdb = AmesPAHdb(filename=xml,
 uids=pahdb.search(f"charge={charge}") #return udis
 b=pahdb.getspeciesbyuid(uids)
 print(f'numbers of selection: {len(uids)}')
-exit(1)
 #convert dict to json
 print('building database')
 spec=b.data
@@ -27,18 +23,13 @@ json_object = json.dumps(spec, separators=(',', ':')) #write to one line string
 with open (js,'w') as f:
     f.write(json_object) #the json file for read structure to xyz file
 print('done')
-"""
-with open (js) as f:
-    data = json.load(f)
-print(data['493']["symmetry"])
-exit(1)
 
+"""
+#### to exzact spectial uid which you want to xyz file ######
 for uid in uids:
     pos=data[f'{uid}']["geometry"]
     formula=data[f'{uid}']["formula"]
     method = data[f'{uid}']['method']
-    print(data['2298']['n_quartet'],data['2298']['n_quintet'],data['2298']['weight'])
-    exit(1)
     print(f"uid: {uid} , {formula}")
     if '+' in formula or '-' in formula:
       formula=formula.replace(formula[-1], '')
@@ -49,10 +40,11 @@ for uid in uids:
         type=pos[i]["type"]
         types.append(int(type))
         coords.append(coord)
-    atom=Atoms(positions=coords,numbers=types,info={'q':q})
+    atom=Atoms(positions=coords,numbers=types)
     if method=='UB3LYP':
-    	write(f'xyz_charge{charge}/mult2/{uid}.xyz',atom)
-    	print(f'{uid} write to mult2')
+    	write(f'xyz_charge{charge}/others/{uid}.xyz',atom) 
+    	print(f'{uid} write to no_1_mult')
     else: 	
         write(f'xyz_charge{charge}/mult1/{uid}.xyz',atom)
         print(f'{uid} write to mult1')
+"""

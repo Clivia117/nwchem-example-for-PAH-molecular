82fa417eb606ff2065cf3cbf3d37160bf058beda
Clivia117 <80371902+Clivia117@users.noreply.github.com>
2023-06-16 10:43:15 +0200

    Update choiceuidinxyz.sh

--- choiceuidinxyz.sh
+++ choiceuidinxyz.sh
@@ -1,14 +1,14 @@
-uids=`awk '{print $1}' ../database/uid4charge0_mult2.dat`
+uids=`awk '{print $1}' ../database/uid4charge0_mult1.dat`
 
 for uid in $uids
 
 do
 
-a=`awk 'NR<2  {print $0}' ../database/xyz_charge0/$uid.xyz`
+a=`awk 'NR<2  {print $0}' ../database/xyz_charge0/$uid.xyz` #read from xyz file
 
 if [ $a -gt 40 -a $a -le 50 ] 
 then
-echo $uid $a
+echo $uid $a >> choicedxyz_mult1.dat
 fi
 
 done
